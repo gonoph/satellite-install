b335e615b58e741a0e5630e33d2b0c9587021be1
Billy Holmes <billy@gonoph.net>
2016-04-16 02:19:54 -0400

    updated bugs in scripts

--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,1 +1,4 @@
-restorecon -Rv /var/lib/mongodb/ /var/lib/pgsql/
+restorecon -Rv /var
+restorecon -Rv /var/lib/mongodb/
+restorecon -Rv /var/lib/pgsql/
+restorecon -Rv /var/lib/pulp
--- scripts/satellite-install-pre.sh
+++ scripts/satellite-install-pre.sh
@@ -5,11 +5,11 @@ subscription-manager repos --disable "*"
 subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-rh-common-rpms
 yum update -y
 subscription-manager repos --enable rhel-server-rhscl-7-rpms --enable rhel-7-server-satellite-6.1-rpms
-yum -y install katello
 yum erase -y ntp ntpdate
 yum install chrony
 systemctl enable chronyd
 systemctl start chronyd
+yum -y install katello
 firewall-cmd --add-port="53/udp" --add-port="53/tcp" \
  --add-port="67/udp" --add-port="68/udp" \
  --add-port="69/udp" --add-port="80/tcp" \

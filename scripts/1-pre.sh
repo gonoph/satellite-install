d3469adb4e2210a5ba8899ff3ad490429d1e136f
Billy Holmes <billy@gonoph.net>
2016-05-02 19:36:35 -0400

    updated pre script to register system

--- scripts/1-pre.sh
+++ scripts/1-pre.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-subscription-manager register --activationkey=GONOPH-NET-SAT --org=7258761
+subscription-manager list || subscription-manager register --activationkey=GONOPH-NET-SAT --org=7258761
 subscription-manager release --set=7Server
 subscription-manager repos --disable "*"
 subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-rh-common-rpms

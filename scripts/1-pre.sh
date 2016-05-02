82ec7999eb527a0df4fcd373924d37cfb5c92985
Billy Holmes <billy@gonoph.net>
2016-05-02 19:35:42 -0400

    updated pre script to register system

--- scripts/1-pre.sh
+++ scripts/1-pre.sh
@@ -2,6 +2,7 @@
 
 set -e 
 
+subscription-manager register --activationkey=GONOPH-NET-SAT --org=7258761
 subscription-manager release --set=7Server
 subscription-manager repos --disable "*"
 subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-rh-common-rpms
@@ -16,10 +17,10 @@ firewall-cmd --add-port="53/udp" --add-port="53/tcp" \
  --add-port="69/udp" --add-port="80/tcp" \
  --add-port="443/tcp" --add-port="5647/tcp" \
  --add-port="8140/tcp" \
-&& firewall-cmd --permanent --add-port="53/udp" --add-port="53/tcp" \
+firewall-cmd --permanent --add-port="53/udp" --add-port="53/tcp" \
  --add-port="67/udp" --add-port="68/udp" \
  --add-port="69/udp" --add-port="80/tcp" \
  --add-port="443/tcp" --add-port="5647/tcp" \
  --add-port="8140/tcp"
 
-yum install katello
+yum -y install katello

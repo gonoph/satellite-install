3b394649514d7eda12b1153d310b858d6bd53f0e
Billy Holmes <billy@gonoph.net>
2016-05-02 19:47:09 -0400

    updated scripts to have a bootstrap

--- scripts/0-init.sh
+++ scripts/0-init.sh
@@ -1,16 +1,11 @@
-systemctl stop postgresql mongod qpidd httpd puppet
-systemctl stop postgresql mongod qpidd httpd puppet
-sync
-find /etc/pki/katello -type f | xargs rm -fv
-find /etc/pki/katello-certs-tools/ -type f
-find /etc/pki/katello-certs-tools/ -type f | xargs rm -rf
-rm /var/lib/pgsql/data/* -rf
-postgresql-setup initdb
-rm /var/lib/mongodb/* -rfv
-rm /var/lib/qpidd/.qpidd/ /var/lib/qpidd/* -rfv
-rm ssl-build/ -rfv
-sync
+#!/bin/sh
 
-yum erase candlepin  foreman puppet httpd foreman-proxy -y
-rm /etc/candlepin/ /var/lib/foreman /var/lib/puppet -rfv /etc/httpd /etc/puppet /etc/foreman /etc/foreman-proxy
-sync
+set -e 
+
+subscription-manager list || subscription-manager register --activationkey=GONOPH-NET-SAT --org=7258761
+subscription-manager release --set=7Server
+subscription-manager repos --disable "*"
+subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-rh-common-rpms
+yum install -y screen git vim
+
+git clone http://git/git/satellite-install.git

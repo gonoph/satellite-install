183c23ff32727fd02c0d2a2ccb455d5d692573fb
Billy Holmes <billy@gonoph.net>
2016-05-03 23:45:10 -0400

    updated hammer to have more pretty info

--- scripts/hammer.sh
+++ scripts/hammer.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-BETA=
-CDN="https://cdn.redhat.com"
+: ${BETA:=}
 ORG="--organization-id=1"
 # hammer subscription upload --file /root/manifest_2016-04-15.zip --organization-id 1
 
@@ -19,10 +18,14 @@ function hammer_enable() {
   hammer repository-set enable "${ORG}" "${PRODUCT}" $ARGS --name="$NAME"
 }
 
+info() {
+  echo -e "\e[0;34m*\e[32m" "$@" "\e[0m"
+}
+
 SECTION=$1
 : ${SECTION:=-h}
 
-[ -n "$BETA" ] && echo "BETA Mode on" 
+[ -n "$BETA" ] && echo -e "\e[1;31mBETA Mode on\e[0m"  || echo -e "\e[1;34mBETA MODE off\e[0m"
 case $SECTION in
   --help|-h)
     cat<<EOF
@@ -38,20 +41,25 @@ RELEASE="--releasever=7Server"
 BASEARCH="--basearch=x86_64"
 
 if [ "$SECTION" = "all" -o "$SECTION" = "repos" ] ; then
+  info "Enabling repos for \e[1;33m$PRODUCT"
   hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Enterprise Linux 7 Server (RPMs)'
   hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Enterprise Linux 7 Server (Kickstart)'
   hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Enterprise Linux 7 Server - RH Common (RPMs)'
   if [ -z "$BETA" ] ; then
+    info "Enabling non-beta repos"
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Enterprise Linux 7 Server - Optional (RPMs)'
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Enterprise Linux 7 Server - Supplementary (RPMs)'
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Storage Native Client for RHEL 7 (RPMs)'
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'RHN Tools for Red Hat Enterprise Linux 7 Server (RPMs)'
   fi
+  info "Enabling extra repos"
   RELEASE=""
   hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Enterprise Linux 7 Server - Extras (RPMs)'
   if [ -n "$BETA" ] ; then
+    info "Enabling satellite tools beta"
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Satellite Tools 6 Beta (for RHEL 7 Server) (RPMs)'
   else
+    info "Enabling satellite tools 6.1"
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Satellite Tools 6.1 (for RHEL 7 Server) (RPMs)'
   fi
 fi
@@ -59,9 +67,11 @@ fi
 if [ "$SECTION" = "all" -o "$SECTION" = "satellite" ] ; then
   RELEASE=""
   if [ -z "$BETA" ] ; then
+    info "Enabling satellite repos for 6.1"
     PRODUCT='--product=Red Hat Satellite'
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Satellite 6.1 (for RHEL 7 Server) (RPMs)'
   else
+    info "Enabling satellite beta repos"
     PRODUCT='--product=Red Hat Satellite 6 Beta'
     hammer_enable "$ORG" "$PRODUCT" "$BASEARCH" "$RELEASE" 'Red Hat Satellite 6 Beta (for RHEL 7 Server) (RPMs)'
   fi
@@ -71,16 +81,20 @@ fi
 ORG="--organization-id=1"
 
 if [ "$SECTION" = "all" -o "$SECTION" = "repos-extra" ] ; then
+  info "Enabling extra forge repos"
   hammer product create ${ORG} --name='Forge'
   hammer repository create ${ORG} --name='Puppet Forge' --product='Forge' --content-type='puppet' --publish-via-http=true --url=https://forge.puppetlabs.com
 
+  info "Enabling extra EPEL repos"
   hammer product create ${ORG} --name='EPEL'
   hammer repository create ${ORG} --name='EPEL 7 - x86_64' --product='EPEL' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/epel/7/x86_64/
 
+  info "Enabling extra Fedora 22,23 repos"
   hammer product create ${ORG} --name='Fedora'
   hammer repository create ${ORG} --name='Fedora 22 - x86_64' --product='Fedora' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/fedora/linux/releases/22/Everything/x86_64/os/
   hammer repository create ${ORG} --name='Fedora 23 - x86_64' --product='Fedora' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/fedora/linux/releases/23/Everything/x86_64/os/
 
+  info "Enabling extra Fedora 22,23 update repos"
   hammer repository create ${ORG} --name='Fedora 22 Updates - x86_64' --product='Fedora' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/fedora/linux/updates/22/x86_64/
   hammer repository create ${ORG} --name='Fedora 23 Updates - x86_64' --product='Fedora' --content-type='yum' --publish-via-http=true --url=http://dl.fedoraproject.org/pub/fedora/linux/updates/23/x86_64/
 
@@ -90,9 +104,11 @@ fi
 ORG="--organization-id=1"
 
 if [ "$SECTION" = "all" -o "$SECTION" = "sync" ]; then
+  info "Creating sync plan for daily syncs"
   hammer sync-plan create --interval=daily --name='Daily sync' ${ORG} --enabled=yes --sync-date=$(date +%Y-%m-%d)
   hammer sync-plan list ${ORG}
 
+  info "Adding all repos to the sync plan"
   ## add stuff to the sync plan
   hammer product set-sync-plan --sync-plan-id=1 ${ORG} --name='Red Hat Enterprise Linux Server'
   hammer product set-sync-plan --sync-plan-id=1 ${ORG} --name='Forge'
@@ -103,6 +119,7 @@ if [ "$SECTION" = "all" -o "$SECTION" = "sync" ]; then
 fi
 
 if [ "$SECTION" = "all" -o "$SECTION" = "view" ] ; then
+  info "Creating content view for rhel 7"
   # Create a content view for RHEL 7 server x86_64:
   hammer content-view create --name='rhel-7-server-x86_64-cv' ${ORG}
   for i in $(hammer --csv repository list ${ORG} | awk -F, {'print $1'} | tail -n +2 ); do
@@ -111,18 +128,30 @@ if [ "$SECTION" = "all" -o "$SECTION" = "view" ] ; then
 fi
 
 if [ "$SECTION" = "all" -o "$SECTION" = "publish" ] ; then
+  info "publishing rhel7 content view"
   hammer content-view publish --name="rhel-7-server-x86_64-cv" ${ORG} --async
 fi
 
 if [ "$SECTION" = "all" -o "$SECTION" = "provisioning" ] ; then
+  info "Provisioning setting: subnet"
   DNS=
   hammer subnet create --organization-ids=1 --boot-mode=Static --dns-primary=192.168.26.82 --dns-secondary=192.168.25.10 --name=$(hostname -d) --network=192.168.26.0 --mask=255.255.255.0 --gateway=192.168.26.10 --ipam=None --tftp-id=1
+
+  info "Provisioning setting: activation-key"
   hammer activation-key create --organization-id=1 --name=RHEL7-BASE --content-view='Default Organization View' --lifecycle-environment=Library --unlimited-content-hosts
+
+  info "Provisioning setting: hostgroup"
+  hostgroup create --organization-ids=1 --architecture=x86_64 --domain=$(hostname -d) --environment=production --medium=Test62/Library/Red_Hat_Server/Red_Hat_Enterprise_Linux_7_Server_Kickstart_x86_64_7Server --name=RHEL7-Server --operatingsystem='RedHat 7.2' --partition-table='Kickstart default' --puppet-ca-proxy-id=1 --puppet-proxy-id=1 --subnet=$(hostname -d) --root-pass=redhat123
+
+  info "Provisioning setting: adding subscriptions to activation key"
+  SUBSCRIPTION_ID=$(hammer --output=csv subscription list --organization-id=1 | awk -F, '/^Employee/ {print $8}')
+  hammer activation-key add-subscription --organization-id=1 --name=RHEL7-BASE --subscription-id=$(SUBSCRIPTION_ID)
+
+  info "Provisioning setting: adding satellite and common repos to activation key"
   hammer activation-key content-override --name=RHEL7-BASE --value=1 --organization-id=1 --content-label=rhel-7-server-satellite-tools-6-beta-rpms
   hammer activation-key content-override --name=RHEL7-BASE --value=1 --organization-id=1 --content-label=rhel-7-server-rh-common-rpms
   # hammer activation-key content-override --name=RHEL7-BASE --value=1 --organization-id=1 --content-label=rhel-7-server-rpms
-  hostgroup create --organization-ids=1 --architecture=x86_64 --domain=$(hostname -d) --environment=production --medium=Test62/Library/Red_Hat_Server/Red_Hat_Enterprise_Linux_7_Server_Kickstart_x86_64_7Server --name=RHEL7-Server --operatingsystem='RedHat 7.2' --partition-table='Kickstart default' --puppet-ca-proxy-id=1 --puppet-proxy-id=1 --subnet=$(hostname -d) --root-pass=redhat123
-  SUBSCRIPTION_ID=$(hammer --output=csv subscription list --organization-id=1 | awk -F, '/^Employee/ {print $8}')
-  hammer activation-key add-subscription --organization-id=1 --name=RHEL7-BASE --subscription-id=$(SUBSCRIPTION_ID)
+
+  info "Provisioning setting: assigning activation key to hostgroup"
   hammer hostgroup set-parameter --hostgroup=RHEL7-Server --name=kt_activation_keys --value=RHEL7-BASE
 fi

047834979c83e95eeb3922531d8d1b039b4b66ba
billy <billy@gonoph.net>
2016-05-04 23:45:11 -0400

    moved bootstrap up a dir

--- 0-bootstrap.sh
+++ 0-bootstrap.sh
@@ -3,20 +3,38 @@
 set -e 
 
 register_system() {
-
-  if [ -z "$RH_ACTIVATION_KEY" ] || [ -z "$RH_ORG_ID" ] ; then
-    echo "Needed environment variables not set!"
-    echo
-    echo "RH_ACTIVATION_KEY - Red Hat Portal Activiation Key"
-    echo "You can create an Activiation Key via https://access.redhat.com/management/activation_keys"
-    echo
-    echo "RH_ORG_ID - Your Red Hat Organization Id"
-    echo "You can find this by running the following on a portal registered system: "
-    echo "	subscription-manager identity"
-    exit 1
+  if [ -n "$RH_OLD_SYSTEM" ] && [ -n "$RH_USER" ] ; then
+    subscription-manager register --consumerid=$RH_OLD_SYSTEM --username=$RH_USER
+    return
   fi
+  if [ -n "$RH_ACTIVATION_KEY" ] && [ -n $RH_ORG_ID ] ; then
+    subscription-manager register --activationkey=${RH_ACTIVATION_KEY} --org=${RH_ORG_ID}
+    return
+  fi
+  cat<<EOF
+Needed environment variables not set!
 
-  subscription-manager register --activationkey=${RH_ACTIVATION_KEY} --org=${RH_ORG_ID}
+If you want to reuse an existing system:
+
+1) Log into the portal: https://access.redhat.com/management/consumers?type=system
+2) Find the old system, copy it's UUID (ex: ad88c818-7777-4370-8878-2f1315f7177a)
+3) Set these ENV variables:
+
+	RH_OLD_SYSTEM=ad88c818-7777-4370-8878-2f1315f7177a
+	RH_USER=biholmes
+
+However, if you want to use an activation key, you need to do this:
+
+1) On an exsting system, find your Red Hat Organization Id:
+2) Log in and run: subscription-manager identity
+3) Setup an activation key via: https://access.redhat.com/management/activation_keys
+4) Set these ENV variables:
+
+	RH_ACTIVATION_KEY=MY_COOL_KEY
+	RH_ORG_ID=31337
+
+EOF
+  exit 1
 }
 
 subscription-manager identity || register_system

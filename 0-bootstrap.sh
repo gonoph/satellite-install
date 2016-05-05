7bcc371b67f7283c6ab6f0b6e370685dc9b0448c
billy <billy@gonoph.net>
2016-05-05 00:26:25 -0400

    updated bootstrap script

--- 0-bootstrap.sh
+++ 0-bootstrap.sh
@@ -49,21 +49,26 @@ fix_hostname() {
 }
 
 fix_ip() {
-  echo "Determining old ip from hostname..."
+  echo "Determining old ip from hostname: $HOST"
   OLDIP=$(ping -w 1 -c 1 $HOST 2>/dev/null | grep ^PING | tr '()' ',' | cut -d , -f 2)
-  if [ -z "$IP" ] ; then
+  if [ -z "$OLDIP" ] ; then
     echo "Unable to determine old ipaddress"
     return
   fi
   IP_MASK=$(nmcli c show eth0 | grep ipv4.addresses | tr -s ' ' | cut -d ' ' -f 2)
-  MASK=$(cut -d / -f 2 <<< "$OLDIP_MASK")
-  IP=$(cut -d / -f 1 <<< "$OLDIP_MASK")
+  MASK=$(cut -d / -f 2 <<< "$IP_MASK")
+  IP=$(cut -d / -f 1 <<< "$IP_MASK")
   if [ "$IP" = "$OLDIP" ] ; then
     echo "Old ip and current ip are the same."
     return
   fi
+  INTERFACE=$(ip route | grep ^default | sed 's/^.*dev \([[:alnum:]]*\) .*$/\1/')
+  if [ -z "$INTERFACE" ] ; then
+    echo "Unable to find primary ethernet device!"
+    exit 1
+  fi
   echo "Old ip and current ip don't match, setting ip to old ip: $OLDIP/$MASK"
-  nmcli c modify ipv4.addresses "$OLDIP/$MASK"
+  nmcli c modify $INTERFACE ipv4.addresses "$OLDIP/$MASK"
 }
 
 subscription-manager identity || register_system
@@ -74,4 +79,31 @@ subscription-manager repos --disable "*"
 subscription-manager repos --enable rhel-7-server-rpms --enable rhel-7-server-rh-common-rpms
 yum install -y screen git vim
 
-git clone http://git/git/satellite-install.git
+[ -r satellite-install/.git ] || git clone http://git/git/satellite-install.git
+
+(
+  cd satellite-install
+  git pull
+)
+
+if fgrep -q nfs /etc/fstab ; then
+  echo "You have NFS mounts, you should probably make sure they're good."
+  grep nfs /etc/fstab
+  read -p "Edit now? " YN
+  case $YN in
+    y|Y|[yY][eE][sS])
+      vim /etc/fstab
+      ;;
+  esac
+fi
+
+if [ -n "$INTERFACE" ] ; then
+  echo "IP address changed! You should reboot!"
+  read -p "Reboot now? " YN
+  case $YN in
+    y|Y|[yY][eE][sS])
+      systemctl reboot;;
+  esac
+  echo
+  echo "Ok, but you need to reboot soon!"
+fi
